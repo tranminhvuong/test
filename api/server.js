53e4be70e16aeb38d82c2ba4e086ec17f6ba7d65
Tran Minh Vuong <minhvuonga31996@gmail.com>
2024-07-27 19:02:17 +0700

    deploy vercel

--- api/server.js
+++ api/server.js
@@ -1,15 +1,23 @@
 const jsonServer = require("json-server");
+const fs = require('fs');
+const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router("/tmp/db.json");
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
+fs.copyFile(path.join(__dirname, '../tmp/db.json'), '/tmp/db.json', (err) => {
+  if (err) throw err;
+  const router = jsonServer.router("/tmp/db.json");
+  const middlewares = jsonServer.defaults();
 
-server.listen(8080, () => {
-  console.log("JSON Server is running");
+  server.use(middlewares);
+  server.use(router);
+
+  server.listen(8080, () => {
+    console.log("JSON Server is running");
+  });
 });
+
+
 
 // Export the Server API
 module.exports = server;
